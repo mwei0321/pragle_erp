f664251c5998b11e8d0d880d787283ebfc50e737
MaWei <1123265518@qq.com>
2022-08-31 23:31:02 +0800

    goods

---
+++ controllers/erpapi/GoodsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers\erpapi;
+
+use app\controllers\InitController;
+use GuzzleHttp\Psr7\Query;
+use system\common\{ServiceFactory, TableMap};
+use system\traits\BindBeanParamsTrait;
+use system\beans\consult\ConsultBeans;
+
+class GoodsController extends InitController
+{
+
+
+    function actionIndex() {
+        
+    }
+}
